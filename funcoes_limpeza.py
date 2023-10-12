196e7d1d5e2d65d339c69ea227bf61660971bb0a
Kaiky Braga <126474113+KaikyBraga@users.noreply.github.com>
2023-10-11 22:40:54 -0300

    Atualizações na função limpa_ALTURA

--- funcoes_limpeza.py
+++ funcoes_limpeza.py
@@ -11,6 +11,7 @@ def limpa_PESO(df):
     Returns:
         df_tratado(dataframe): DataFrame com linhas indesejadas removidas.
     """
+    # Criação de cópia do DataFrame, sem alterar o original
     df_tratado = df.copy()
     try:
         # Conversão da coluna PESO para o tipo numérico
@@ -19,18 +20,37 @@ def limpa_PESO(df):
         print("A coluna 'PESO' não está presente no DataFrame.")
     except ValueError:
         print("Impossível converter a coluna 'PESO' para o tipo numérico.")
-    # Limpando a coluna PESO usando df_tratado em vez de df
+    # Limpeza da coluna 'PESO'
     df_tratado = df_tratado.dropna(subset=["PESO"])
     df_tratado = df_tratado.loc[df_tratado["PESO"] >= 40]
     df_tratado = df_tratado.loc[df_tratado["PESO"] <= 200]
     return df_tratado
 
 def limpa_ALTURA(df):
-    #Limpando coluna ALTURA
-    df = df.dropna(subset=['ALTURA'])
-    df = df.loc[df['ALTURA'] >= 140]
-    df = df.loc[df['ALTURA'] <= 220]
-    return df
+    """
+    Realiza a remoção de linhas do DataFrame de acordo com a coluna 'ALTURA',
+    permanecendo apenas dados não nulos e em uma faixa de valor entre 140 e 220 centímetros.
+
+    Parameters:
+        df(dataframe): DataFrame a ser processado.
+
+    Returns:
+        df_tratado(dataframe): DataFrame com linhas indesejadas removidas.
+    """
+    # Criação de cópia do DataFrame, sem alterar o original
+    df_tratado = df.copy()
+    try:
+        # Conversão da coluna ALTURA para o tipo numérico
+        df_tratado["ALTURA"] = pd.to_numeric(df_tratado["ALTURA"], errors="coerce")
+    except KeyError:
+        print("A coluna 'ALTURA' não está presente no DataFrame.")
+    except ValueError:
+        print("Impossível converter a coluna 'ALTURA' para o tipo numérico.")
+    # Limpeza da coluna 'ALTURA'
+    df_tratado = df_tratado.dropna(subset=['ALTURA'])
+    df_tratado = df_tratado.loc[df_tratado['ALTURA'] >= 140]
+    df_tratado = df_tratado.loc[df_tratado['ALTURA'] <= 220]
+    return df_tratado
 
 def limpa_SEXO(df):
     """
@@ -63,7 +83,7 @@ def exclui_colunas(df):
         df_tratado(dataframe): DataFrame sem as colunas especificadas.
     """
     try:
-        # Excluindo colunas desnecessárias sem alterar o DataFrame original
+        # Exclusão de colunas desnecessárias sem alteração no DataFrame original
         df_tratado = df.copy()
         df_tratado = df_tratado.drop(["CABECA", "CALCADO", "CINTURA", "JSM",
                                       "MUN_JSM", "UF_JSM", "RELIGIAO"], axis=1)
