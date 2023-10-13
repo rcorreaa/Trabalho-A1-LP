3c319cc216dc6497ed8cf5de4c6c2a759b6e7be7
Ramyro Corrêa Aquines <ramyrocorrea@gmail.com>
2023-10-13 00:35:33 -0300

    Fiz minha visualização com base nos anos escolhidos

--- limpeza.py
+++ limpeza.py
@@ -1,12 +1,20 @@
 def limpeza(caminho_csv):
-    import pandas as pd #Importo o Pandas
-    df = pd.read_csv(caminho_csv, sep=";") #Leio o arquivo CSV a ser analisado
-    df_limpo = df.copy() #Crio uma cópia do meu Dataframe original
-    #Removo colunas que não serão utilizadas na visualização
+    '''
+    Args:
+    caminho_csv: O caminho do arquivo CSV a ser lido.
+    
+    Returns:
+    df_limpo: O dataframe original após a limpeza dos dados.
+    
+    Exemplo:
+        >>> caminho_arquivo = "dados.csv"
+        >>> df_limpo = limpeza(caminho_arquivo)
+        >>> print(df_limpo.head())
+    '''
+    import pandas as pd #Importo o Pandas.
+    df = pd.read_csv(caminho_csv, sep=";") #Leio o arquivo CSV a ser analisado.
+    df_limpo = df.copy() #Crio uma cópia do meu Dataframe original.
+    #Removo colunas que não serão utilizadas na visualização.
     df_limpo = df_limpo.drop(columns=["RELIGIAO", "SEXO", "MUN_NASCIMENTO", "PAIS_NASCIMENTO", "ESTADO_CIVIL", "VINCULACAO_ANO", "MUN_RESIDENCIA", "JSM", "UF_JSM", "MUN_JSM"])
-    #Removo linhas com ao menos 2 itens em branco
-    df_limpo = df_limpo.dropna(thresh =df.shape[1]-1)
-    #As células nulas na coluna altura (a única com células nulas) são trocadas pela mediana da altura
-    m = df_limpo["ALTURA"].median() #Encontro a mediana
-    df_limpo["ALTURA"].fillna(m, inplace = True) #Executo o desejado
-    return df_limpo #Retorno o Dataframe limpo
+    df_limpo = df_limpo.dropna() #Removo linhas com ao menos 1 item em branco.
+    return df_limpo #Retorno o Dataframe limpo.
