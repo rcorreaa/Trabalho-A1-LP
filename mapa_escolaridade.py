1ca9e33881f43b405b9de3cf84569a4c8f08ba16
Kaiky Braga <126474113+KaikyBraga@users.noreply.github.com>
2023-10-13 18:53:50 -0300

    Criação da função do gráfico do mapa

--- mapa_escolaridade.py
+++ mapa_escolaridade.py
@@ -3,38 +3,49 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.colors import to_rgba
 
-# Carregar dados geoespaciais do Brasil
-brasil = gpd.read_file("BR_UF_2021.zip", compression="zip", encoding="utf-8")
 
-# Carregar dados da tabela de escolaridade
-df_mapa = pd.read_csv("tabela_escolaridade.csv")
-df_mapa = df_mapa.rename(columns={"UF_RESIDENCIA": "SIGLA"})
+def plot_grafico_mapa(pathdata="tabela_escolaridade.csv", pathgeografia="BR_UF_2021.zip"):
+    """
+    Plota o grafico de violino do imc entre os anos ini_ano e fim_ano.
 
-# Reoordenando o DataFrame de acordo com o nível de escolaridade
-df_mapa = df_mapa.sort_values(by="NIVEL_ESCOLARIDADE")
+    Parameters:
+        path_data(string): diretório dos arquivos .csv
+        pathgeografia(string): diretório do arquivo .zip dos dados geográficos do Brasil
+    Returns:
+        void
+    """
+    # Carregar dados geoespaciais do Brasil
+    brasil = gpd.read_file(pathgeografia, compression="zip", encoding="utf-8")
 
-# Normalização dos valores do nível de escolaridade
-df_mapa["NIVEL_ESCOLARIDADE"] = df_mapa["NIVEL_ESCOLARIDADE"] / 100
+    # Carregar dados da tabela de escolaridade
+    df_mapa = pd.read_csv(pathdata)
+    df_mapa = df_mapa.rename(columns={"UF_RESIDENCIA": "SIGLA"})
 
-# Criação de tabela de cor gradiente dos níveis de escolaridade
-df_mapa["COR_GRADIENTE"] = [to_rgba(plt.cm.Reds(value)) for value in df_mapa["NIVEL_ESCOLARIDADE"]]
-df_mapa = df_mapa.set_index("SIGLA").reindex(brasil["SIGLA"]).reset_index()
+    # Reoordenando o DataFrame de acordo com o nível de escolaridade
+    df_mapa = df_mapa.sort_values(by="NIVEL_ESCOLARIDADE")
 
-# Personalização do mapa
-janela, graf_mapa = plt.subplots(1, 1, figsize=(10, 10))
-brasil.plot(ax=graf_mapa, color=df_mapa["COR_GRADIENTE"], edgecolor="black", linewidth=0.7)
-graf_mapa.set_title("Nível de Escolaridade por Estado", fontdict={"fontsize": "15", "fontname": "Arial"})
+    # Normalização dos valores do nível de escolaridade
+    df_mapa["NIVEL_ESCOLARIDADE"] = df_mapa["NIVEL_ESCOLARIDADE"] / 100
 
-# Rótulos nos eixos
-graf_mapa.set_xlabel("Latitude", fontdict={"fontsize": "12", "fontname": "Arial"})
-graf_mapa.set_ylabel("Longitude", fontdict={"fontsize": "12", "fontname": "Arial"})
+    # Criação de tabela de cor gradiente dos níveis de escolaridade
+    df_mapa["COR_GRADIENTE"] = [to_rgba(plt.cm.Reds(value)) for value in df_mapa["NIVEL_ESCOLARIDADE"]]
+    df_mapa = df_mapa.set_index("SIGLA").reindex(brasil["SIGLA"]).reset_index()
 
-# Cores dos paineis
-graf_mapa.set_facecolor("#8CBCDB")
-janela.set_facecolor("gray")
+    # Personalização do mapa
+    janela, graf_mapa = plt.subplots(1, 1, figsize=(10, 10))
+    brasil.plot(ax=graf_mapa, color=df_mapa["COR_GRADIENTE"], edgecolor="black", linewidth=0.7)
+    graf_mapa.set_title("Nível de Escolaridade por Estado", fontdict={"fontsize": "15", "fontname": "Arial"})
 
-# Demarcação do gráfico (linhas de latitude e longitude)
-graf_mapa.set_xticks([-70, -60, -50, -40, -30])
-graf_mapa.set_yticks([-30, -20, -10, 0])
+    # Rótulos nos eixos
+    graf_mapa.set_xlabel("Latitude", fontdict={"fontsize": "12", "fontname": "Arial"})
+    graf_mapa.set_ylabel("Longitude", fontdict={"fontsize": "12", "fontname": "Arial"})
 
-plt.show()
+    # Cores dos paineis
+    graf_mapa.set_facecolor("#8CBCDB")
+    janela.set_facecolor("gray")
+
+    # Demarcação do gráfico (linhas de latitude e longitude)
+    graf_mapa.set_xticks([-70, -60, -50, -40, -30])
+    graf_mapa.set_yticks([-30, -20, -10, 0])
+
+    plt.show()
