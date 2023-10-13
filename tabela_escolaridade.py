c9de16365d8be6a45444c61b1e5d2ce8405a0427
Kaiky Braga <126474113+KaikyBraga@users.noreply.github.com>
2023-10-13 19:21:30 -0300

    Adição de função da tabela de escolaridade

--- tabela_escolaridade.py
+++ tabela_escolaridade.py
@@ -1,51 +1,56 @@
-from funcoes_limpeza import *
+def tabela_escolaridade(pathdata, ini_ano=2018, fim_ano=2022):
+    """
+    A função tem por objetivo gerar um arquivo csv dos dados da média dos estados de
+    escolaridade do Brasil do período passado. Por padrão, de 2018 até 2022.
 
-df_2022 = pd.read_csv("sermil2022.csv")
-df_2021 = pd.read_csv("sermil2022.csv")
-df_2020 = pd.read_csv("sermil2022.csv")
-df_2019 = pd.read_csv("sermil2022.csv")
-df_2018 = pd.read_csv("sermil2022.csv")
+    Parameters:
+        path_data(string): diretório dos arquivos dos arquivos .csv
+        ini_ano(int): ano de inicio. 2018 por padrão
+        fim_ano(int): ano do final. 2022 por padrão
+    Returns:
+        tabela_escolaridade_csv(csv): csv utilizado para o mapa.
+    """
+    v_df = []
 
-# Filtrando os DataFrames por colunas desejadas
-df_2022 = df_2022[["UF_RESIDENCIA", "ESCOLARIDADE"]]
-df_2021 = df_2021[["UF_RESIDENCIA", "ESCOLARIDADE"]]
-df_2020 = df_2020[["UF_RESIDENCIA", "ESCOLARIDADE"]]
-df_2019 = df_2019[["UF_RESIDENCIA", "ESCOLARIDADE"]]
-df_2018 = df_2018[["UF_RESIDENCIA", "ESCOLARIDADE"]]
+    for ano in range(ini_ano, fim_ano + 1):
+        # leitura do database com tratamento de erro
+        try:
+            df = pd.read_csv(path_data + "sermil{}.csv".format(ano), usecols=["UF_RESIDENCIA", "ESCOLARIDADE"])
+        except UnicodeDecodeError:
+            df = pd.read_csv(path_data + "sermil{}.csv".format(ano), usecols=["UF_RESIDENCIA", "ESCOLARIDADE"], encoding="latin1")
+        except:
+            print("Erro de leitura no database do ano", ano)
+            return
 
-# Renomeando os registros da coluna ESCOLARIDADE
-df_2022 = renomeia_ESCOLARIDADE(df_2022)
-df_2021 = renomeia_ESCOLARIDADE(df_2022)
-df_2020 = renomeia_ESCOLARIDADE(df_2022)
-df_2019 = renomeia_ESCOLARIDADE(df_2022)
-df_2018 = renomeia_ESCOLARIDADE(df_2022)
+        # Agrupando dados da coluna de escolaridade e criando coluna de nível de escolaridade
+        df = renomeia_ESCOLARIDADE(df)
+        df = nivel_ESCOLARIDADE(df)
 
-# Adição da da coluna NIVEL DE ESCOLARIDADE nos DATAFRAMES
-df_2022 = nivel_ESCOLARIDADE(df_2022)
-df_2021 = nivel_ESCOLARIDADE(df_2021)
-df_2020 = nivel_ESCOLARIDADE(df_2020)
-df_2019 = nivel_ESCOLARIDADE(df_2019)
-df_2018 = nivel_ESCOLARIDADE(df_2018)
+        v_df.append(df)
 
-# Junção dos DataFrames verticalmente
-df_concatenado = pd.concat([df_2022, df_2021, df_2020, df_2019, df_2018], ignore_index=True)
+    # Junção dos DataFrames verticalmente
+    df_concatenado = pd.concat(v_df, ignore_index=True)
 
-# Remoção da coluna de escolaridade
-df_concatenado.drop("ESCOLARIDADE", axis=1, inplace=True)
+    # Remoção da coluna de escolaridade
+    df_concatenado.drop("ESCOLARIDADE", axis=1, inplace=True)
 
-# Média de escolaridade por estado
-media_escolaridade_estados = df_concatenado.groupby("UF_RESIDENCIA")["NIVEL_ESCOLARIDADE"].mean().reset_index()
+    # Média de escolaridade por estado
+    media_escolaridade_estados = df_concatenado.groupby("UF_RESIDENCIA")["NIVEL_ESCOLARIDADE"].mean().reset_index()
 
-# Remoção do estado 'KK' e resetação de índices
-media_escolaridade_estados = media_escolaridade_estados.loc[media_escolaridade_estados["UF_RESIDENCIA"] != "KK"]
-media_escolaridade_estados = media_escolaridade_estados.reset_index(drop=True)
+    # Remoção do estado 'KK' e resetação de índices
+    media_escolaridade_estados = media_escolaridade_estados.loc[media_escolaridade_estados["UF_RESIDENCIA"] != "KK"]
+    media_escolaridade_estados = media_escolaridade_estados.reset_index(drop=True)
 
-# Adição de proporção proporção no nível de escolaridade através da função exponencial
-media_escolaridade_estados["NIVEL_ESCOLARIDADE"] = 2.5**media_escolaridade_estados["NIVEL_ESCOLARIDADE"]
+    # Adição de proporção proporção no nível de escolaridade através da função exponencial
+    media_escolaridade_estados["NIVEL_ESCOLARIDADE"] = 2.5**media_escolaridade_estados["NIVEL_ESCOLARIDADE"]
 
-# Extração de máximo e mínimo do Data
-escolaridade_maxima = media_escolaridade_estados["NIVEL_ESCOLARIDADE"].max()
-escolaridade_minima = media_escolaridade_estados["NIVEL_ESCOLARIDADE"].min()
+    # Extração de máximo e mínimo do Data
+    escolaridade_maxima = media_escolaridade_estados["NIVEL_ESCOLARIDADE"].max()
+    escolaridade_minima = media_escolaridade_estados["NIVEL_ESCOLARIDADE"].min()
 
-# Normalização do nível de escolaridade na escala de 0 a 100
-media_escolaridade_estados["NIVEL_ESCOLARIDADE"] = (media_escolaridade_estados["NIVEL_ESCOLARIDADE"] - escolaridade_minima)/(escolaridade_maxima-escolaridade_minima)*100
+    # Normalização do nível de escolaridade na escala de 0 a 100
+    media_escolaridade_estados["NIVEL_ESCOLARIDADE"] = (media_escolaridade_estados["NIVEL_ESCOLARIDADE"] - escolaridade_minima)/(escolaridade_maxima-escolaridade_minima)*100
+
+    tabela_escolaridade_csv = media_escolaridade_estados.to_csv(index=False)
+
+    return tabela_escolaridade_csv
