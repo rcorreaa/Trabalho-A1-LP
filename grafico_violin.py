ea5ac38cc43e00667f623b05f94baa50654ad88f
falcaoanderson <andersonfalcaosantos@gmail.com>
2023-10-13 12:09:45 -0300

    modularizacao do grafico_imc_violin

--- grafico_violin.py
+++ grafico_violin.py
@@ -3,45 +3,46 @@ import numpy as np
 import matplotlib.pyplot as plt
 from funcoes_limpeza import limpa_PESO, limpa_ALTURA, limpa_SEXO, limpa_ANO_NASCIMENTO
 
-v_df = []
-ini_ano = 2016
-fim_ano = 2022
+def plot_grafico_imc_violin(path_data, ini_ano=2013, fim_ano=2022):
+    v_df = []
 
-for ano in range(ini_ano, fim_ano+1):
-    try:
-        df = pd.read_csv("../lp/data/sermil{}.csv".format(ano), usecols=["PESO", "ALTURA", "SEXO", "ANO_NASCIMENTO"])
-    except UnicodeDecodeError:
-        df = pd.read_csv("../lp/data/sermil{}.csv".format(ano), usecols=["PESO", "ALTURA", "SEXO", "ANO_NASCIMENTO"], encoding="latin1")
-    except:
-        print("Erro de leitura do database")
-        continue
+    for ano in range(ini_ano, fim_ano+1):
+        try:
+            df = pd.read_csv(path_data + "sermil{}.csv".format(ano), usecols=["PESO", "ALTURA", "SEXO", "ANO_NASCIMENTO"])
+        except UnicodeDecodeError:
+            df = pd.read_csv(path_data + "sermil{}.csv".format(ano), usecols=["PESO", "ALTURA", "SEXO", "ANO_NASCIMENTO"], encoding="latin1")
+        except:
+            print("Erro de leitura no database do ano", ano)
+            return
 
-    df = limpa_PESO(df)
-    df = limpa_ALTURA(df)
-    df = limpa_SEXO(df)
-    df = limpa_ANO_NASCIMENTO(df, ano)
-    
-    df["IMC"] = df["PESO"] / ((df["ALTURA"]/100)**2)
-    df = df["IMC"]
+        df = limpa_PESO(df)
+        df = limpa_ALTURA(df)
+        df = limpa_SEXO(df)
+        df = limpa_ANO_NASCIMENTO(df, ano)
+        
+        df["IMC"] = df["PESO"] / ((df["ALTURA"]/100)**2)
+        df = df["IMC"]
 
-    v_df.append(df)
-    
-    print(ano)
-    print(df.describe())
-    print()
+        v_df.append(df)
+        
+        print(ano)
+        print(df.describe())
+        print()
 
 
-fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(14, 6))
+    fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(14, 6))
 
-axs[0].violinplot(v_df, showmedians=True)
-axs[1].boxplot(v_df, sym="")
+    axs[0].violinplot(v_df, showmedians=True)
+    axs[1].boxplot(v_df, sym="")
 
-for ax in axs:
-    ax.yaxis.grid(True)
-    
-    ax.set_xticks([y + 1 for y in range(len(v_df))], labels=[ano for ano in range(ini_ano, fim_ano+1)])
-    
-    ax.set_xlabel('Ano')
-    ax.set_ylabel('IMC')
+    for ax in axs:
+        ax.yaxis.grid(True)
+        
+        ax.set_xticks([y + 1 for y in range(len(v_df))], labels=[ano for ano in range(ini_ano, fim_ano+1)])
+        
+        ax.set_xlabel('Ano')
+        ax.set_ylabel('IMC')
 
-plt.show()
+    plt.show()
+
+plot_grafico_imc_violin("../lp/data/")
