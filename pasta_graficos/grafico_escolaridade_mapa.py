2e8519ffd8494a73a9faa7886cfe5d20d67ddd0c
Kaiky Braga <126474113+KaikyBraga@users.noreply.github.com>
2023-10-15 17:27:53 -0300

    Adição legenda mapa

--- pasta_graficos/grafico_escolaridade_mapa.py
+++ pasta_graficos/grafico_escolaridade_mapa.py
@@ -55,12 +55,25 @@ def plot_grafico_mapa(path_data, path_geografia):
 
     # Personalização do mapa
     janela, graf_mapa = plt.subplots(1, 1, figsize=(10, 10))
-    brasil.plot(ax=graf_mapa, color=df_mapa["COR_GRADIENTE"], edgecolor="black", linewidth=0.7)
-    graf_mapa.set_title("Nível de Escolaridade por Estado", fontdict={"fontsize": "15", "fontname": "Arial","fontweight": "bold"})
+    brasil.plot(ax=graf_mapa, color=df_mapa["COR_GRADIENTE"],edgecolor="black", linewidth=0.7)
+    graf_mapa.set_title("Nível de Escolaridade por Estado",
+                        fontdict={"fontsize": "15", "fontname": "Arial", "fontweight": "bold"})
+
+    # Barra de legenda à direita do mapa
+    eixos_divisor = make_axes_locatable(graf_mapa)
+    colorbar_axis = eixos_divisor.append_axes("right", size="5%", pad=0.1)
+    cbar = plt.colorbar(plt.cm.ScalarMappable(norm=None, cmap=plt.cm.Reds), cax=colorbar_axis)
+
+    # Rótulos e posições dos ticks na barra de cores
+    ticks = [0, 25, 50, 75, 100]
+    tick_labels = [f'{tick}%' for tick in ticks]
+    cbar.set_ticks([tick / 100 for tick in ticks])
+    cbar.set_ticklabels(tick_labels, fontdict={"fontsize": "10", "fontname": "Arial"})
+    cbar.set_label("Nível", rotation=270, labelpad=15, fontdict={"fontsize": "12", "fontname": "Arial"})
 
     # Rótulos nos eixos
-    graf_mapa.set_xlabel("Latitude", fontdict={"fontsize": "12", "fontname": "Arial"})
-    graf_mapa.set_ylabel("Longitude", fontdict={"fontsize": "12", "fontname": "Arial"})
+    graf_mapa.set_xlabel("Longitude", fontdict={"fontsize": "12", "fontname": "Arial"})
+    graf_mapa.set_ylabel("Latitude", fontdict={"fontsize": "12", "fontname": "Arial"})
 
     # Cores dos paineis
     graf_mapa.set_facecolor("#8CBCDB")
