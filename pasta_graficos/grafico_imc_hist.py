c4d65bd4c990834a63cbb92cd55185b10dfb4bd4
Kaiky Braga <126474113+KaikyBraga@users.noreply.github.com>
2023-10-16 08:03:30 -0300

    Documentação do módulo grafico_imc_hist.py

--- pasta_graficos/grafico_imc_hist.py
+++ pasta_graficos/grafico_imc_hist.py
@@ -3,6 +3,10 @@ import pandas as pd
 from pasta_funcoes.funcoes_limpeza  import limpa_PESO, limpa_ALTURA, limpa_ANO_NASCIMENTO, limpa_SEXO
 import doctest
 
+"""
+Módulo da visualiação do Gráfico de Histograma feito pelo integrante Ramyro. 
+"""
+
 def plot_grafico_histograma(path_data, ano1=2010, ano2=2014, ano3=2014, ano4=2022):
     """
     Plota o histograma com as densidades dos IMCs dentre 4 anos selecionados.
